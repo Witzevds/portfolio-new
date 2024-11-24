966eb6fc33b11510283c2d79684018a01cfe24fb
Witzevds <witzvand@student.arteveldehs.be>
2024-11-24 20:15:15 +0100

    cool animations added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 
 import viteLogo from "/vite.svg";
 import "./assets/styles/globals.css";
+import "./assets/styles/Variables.css";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
+import About from "./components/About/About.jsx";
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-import "./Hero.css"; // Dit is het CSS-bestand
+import "./Hero.css";
 
 const Hero = () => {
+  const title = "front end dev."; // Define your title string
+
   return (
-    <>
-      <div className="hero">
-        <h1 className="hero__title">
-          front <br />
-          end <br />
-          dev
-        </h1>
-        <div className="hero__description"></div>
+    <div className="hero">
+      <h1 className="hero__title">
+        {title.split(" ").map((word, index) => (
+          <span key={index} className="char" style={{ "--char-index": index }}>
+            {word}
+            <br /> {/* Add a new line after each word */}
+          </span>
+        ))}
+      </h1>
+      <div className="hero__description">
         <p className="hero__description--text">
           Welcome to my portfolio! I’m Witze Van der Straeten, a frontend
           development student at Artevelde Hogeschool, focused on building
@@ -22,7 +25,7 @@ const Hero = () => {
           contact me
         </a>
       </div>
-    </>
+    </div>
   );
 };
 
