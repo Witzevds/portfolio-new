00d7d307ad62fac8b44a6a271fe9131139c7e4e4
Witzevds <witzvand@student.arteveldehs.be>
2024-11-24 19:31:16 +0100

    test render

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import viteLogo from "/vite.svg";
+import "./assets/styles/globals.css";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import "./Hero.css"; // Dit is het CSS-bestand
 
 const Hero = () => {
-  return <p>test</p>;
+  return (
+    <>
+      <div className="hero">
+        <h1 className="hero__title">
+          front <br />
+          end <br />
+          dev
+        </h1>
+        <div className="hero__description"></div>
+        <p className="hero__description--text">
+          Welcome to my portfolio! I’m Witze Van der Straeten, a frontend
+          development student at Artevelde Hogeschool, focused on building
+          clean, responsive websites. Explore my projects to see my skills and
+          passion for web development in action!
+        </p>
+
+        <a className="hero__description--btn" href="">
+          contact me
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
