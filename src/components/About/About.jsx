229c87f39c0059e521879fdf286ca228d843a9cd
Witzevds <witzvand@student.arteveldehs.be>
2024-11-24 23:09:04 +0100

    added about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,55 @@
+import React from "react";
+import "./About.css";
+
 const About = () => {
   return (
     <div className="about">
-      <h1>About</h1>
+      <div className="about__left">
+        <h1 className="about__left--title">Who am I</h1>
+        <div className="about__image-description">
+          <img
+            className="about__left--image"
+            src="src/assets/images/witze-portret.jpg"
+            alt="Witze Van der Straeten"
+          />
+          <div className="about__left--description">
+            <p>
+              I’m Witze Van der Straeten, a frontend development student with a
+              background in design. I first studied design, but my passion
+              quickly shifted to web development. My main tools are HTML, CSS,
+              JavaScript, and React, and my design skills give me a strong eye
+              for detail.
+            </p>
+            <p>
+              Beyond coding, I enjoy going to the gym and reading books—two
+              things that keep me balanced and inspired. My goal is to
+              eventually start my own business with a small, dedicated team. I
+              thrive in collaborative settings but can work independently just
+              as well.
+            </p>
+          </div>
+        </div>
+        <div className="skills">
+          <h2>Skills</h2>
+          <div className="skills-icons">
+            <img src="src/assets/images/react.svg" alt="React" />
+            <img src="src/assets/images/eleventy.svg" alt="Eleventy" />
+            <img src="src/assets/icons/html5.svg" alt="HTML5" />
+            <img src="src/assets/images/css.svg" alt="CSS3" />
+            <img src="src/assets/icons/figma.svg" alt="Figma" />
+            <img src="src/assets/icons/photoshop.svg" alt="Photoshop" />
+            <img src="src/assets/images/html.svg" alt="Html" />
+            <img src="src/assets/icons/sql.svg" alt="SQL" />
+          </div>
+        </div>
+      </div>
+      <div className="about__right">
+        <img
+          className="about__right--image"
+          src="src/assets/images/10723.jpg"
+          alt="Background Illustration"
+        />
+      </div>
     </div>
   );
 };
