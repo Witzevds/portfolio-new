a838fce0f29d63ebf85ee32ab6967bbc33d7415f
Witzevds <witzvand@student.arteveldehs.be>
2024-11-25 00:28:11 +0100

    added footer not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import About from "./components/About/About.jsx";
 import Projects from "./components/Projects/Projects.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Footer />
     </>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,9 +4,9 @@ const Projects = () => {
   return (
     <>
       <div className="projects">
-        <Card image={"ui-illustration.svg"} title={"illustrations"} />
-        <Card image={"web-development.png"} title={"Web development"} />
+        <Card image={"ui-illustration.svg"} title={"UI & UX"} />
         <Card image={"illustrations.png"} title={"illustrations"} />
+        <Card image={"web-development.png"} title={"Web development"} />
       </div>
     </>
   );
