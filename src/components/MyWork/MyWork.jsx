1fe270758eac77346e6521e3ab2785a2068bb5c2
Witzevds <witzvand@student.arteveldehs.be>
2024-11-26 21:32:08 +0100

    added router

---
+++ src/components/MyWork/MyWork.jsx
@@ -0,0 +1,19 @@
+import "./MyWork.css";
+
+const MyWork = () => {
+  return (
+    <div className="my-work">
+      <h2 className="my-work__title">UI-UX</h2>
+      <div className="my-work__content">
+        <div className="my-work__card">
+          <h3 className="my-work__card-title">UI & UX</h3>
+          <a className="my-work__card-btn" href="">
+            UI & UX
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyWork;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import Projects from "./pages/Projects/Projects.jsx";
+import App from "./pages/App/App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Projects",
+    element: <Projects />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
