b29d3f420e25b36e929fc11ff916adedd6887648
Witzevds <witzvand@student.arteveldehs.be>
2024-12-31 00:57:29 +0100

    enhance ProjectCard component with website and description props; update styles and project list

--- src/components/Project-card/Project-card.jsx
+++ src/components/Project-card/Project-card.jsx
@@ -1,21 +1,17 @@
 import { Link } from "react-router-dom";
 import "./Project-card.css";
 
-const ProjectCard = ({ id, title, image }) => {
+const ProjectCard = ({ id, title, image, website, description }) => {
   return (
     <div className="project-card">
       <h3 className="project-card__title">{title}</h3>
       <img
         className="project-card__image"
-        src={`/assets/images/${image}`}
+        src={`/assets/project-images/${image}`}
         alt={title}
       />
-      <Link
-        to={
-          "https://pgmgent-atwork-1.github.io/opdracht-2-gentse-feesten-Witzevds/"
-        }
-        className="project-card__btn"
-      >
+      <p className="project-card__description">{description}</p>
+      <Link to={website} className="project-card__btn">
         Open Website
       </Link>
     </div>
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,14 +5,31 @@ import "./Projects.css";
 
 const Projects = () => {
   const projectList = [
-    { id: "1", title: "Gentse feesten", image: "ui-illustration.svg" },
-    { id: "2", title: "Festival Planner", image: "festival-planner.svg" },
+    {
+      id: "1",
+      title: "Gentse feesten",
+      image: "gentse-feesten.png",
+      website:
+        "https://pgmgent-atwork-1.github.io/opdracht-2-gentse-feesten-Witzevds/",
+
+      description:
+        "A vibrant and lively festival in Ghent, showcasing music, theater, and cultural events.",
+    },
+    {
+      id: "2",
+      title: "Blackout news",
+      image: "blackout-news.png",
+      website: "https://opdracht-2-react-application-witzevds-app.onrender.com",
+
+      description:
+        "Stay updated with the latest news and events during a blackout, ensuring you are always informed.",
+    },
   ];
 
   return (
     <>
       <Header />
-      <MyWork />
+
       <div className="projects-container">
         {projectList.map((project) => (
           <ProjectCard
@@ -20,6 +37,8 @@ const Projects = () => {
             id={project.id}
             title={project.title}
             image={project.image}
+            website={project.website}
+            description={project.description}
           />
         ))}
       </div>
